70bc396d51189cd80c4350fa4316bf676c7d4b4c
Cptooooooo <cptooooooooo@gmail.com>
2024-03-20 12:44:32 -0600

    Add index page view

--- CarpoolApp/proximitytool/views.py
+++ CarpoolApp/proximitytool/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import CarpoolGroup, Address, Person, Company
 
-# Create your views here.
+def index(request):
+    """ View function for home page of site """
+
+    # Get User's CarpoolGroup id
+    carpool_group_id = 1 # NOTE: Stub value
+
+    # Retrive the user's carpool group record
+    carpool_group = CarpoolGroup.objects.filter(id__exact=carpool_group_id)[0]
+
+    context = {"carpool_group" : carpool_group}
+
+    # Render the HTML template index.html with the data in context variable
+    return render(request, "index.html", context=context)
