e656762f37c8359abc379666790543e6ca04fa6f
Mehtab Singh <mehtab.singh@edu.sait.ca>
2024-03-28 18:46:39 -0600

    Update script.js

--- front-end/script.js
+++ front-end/script.js
@@ -6,21 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
     if(logoutButton) {
         logoutButton.addEventListener('click', function () {
             alert('Logout clicked');
-            // Here you might want to redirect to the login page or perform a logout action
         });
     }
 
     if(uploadButton) {
         uploadButton.addEventListener('click', function () {
             alert('Upload Employee Data clicked');
-            // Implement actual upload functionality
         });
     }
 
     if(downloadButton) {
         downloadButton.addEventListener('click', function () {
             alert('Download Template clicked');
-            // Implement the download functionality
         });
     }
 });
