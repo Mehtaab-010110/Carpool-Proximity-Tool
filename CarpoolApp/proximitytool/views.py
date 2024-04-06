80e27b2dd737947b78ed44890be390a914e17706
Cptooooooo <cptooooooooo@gmail.com>
2024-04-05 19:58:38 -0600

    Add login requirement to index page

--- CarpoolApp/proximitytool/views.py
+++ CarpoolApp/proximitytool/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import CarpoolGroup, Address, Person, Company
 
+@login_required
 def index(request):
     """ View function for home page of site """
 
@@ -9,6 +11,10 @@ def index(request):
 
     # Retrive the user's carpool group record
     carpool_group = CarpoolGroup.objects.filter(id__exact=carpool_group_id)[0]
+
+    # Visit Counter
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {"carpool_group" : carpool_group}
 
